7164ff2ee01174d0078a1e75df5ac0f35e1c491e
mathiasdag <mathias@asso.gd>
2023-09-01 16:23:33 +0200

    ...

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,12 +1,9 @@
-const Webflow = require("webflow-api");
-var isSameDay = require("date-fns/isSameDay");
-var parseISO = require("date-fns/parseISO");
+import Webflow from "webflow-api";
+import { isSameDay, parseISO } from "date-fns";
 
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
@@ -22,73 +19,70 @@ const allowCors = (fn) => async (req, res) => {
   return await fn(req, res);
 };
 
-const handler = (req, res) => {
-  // Initialize  API
+// Handle GET request
+export async function GET(req, res) {
+  // Use uppercase GET here
+  // Initialize API
   const api = new Webflow({
-    token: "f7797288bd0730a647f47476d0b84017c4f3a60711ec5675c27421a287362bc7",
+    token: "2d84f9720ff8aadf4b0b94cc2f0141b0b7131e4300fa18fc8f248acb43f4eb7c",
   });
 
-  // Examples
-  // api.sites().then(sites => console.log(sites));
-  // api.site({ siteId: '64b6afc54c1a9ceff92d8f2b' }).then(site => console.log(site));
-  // api.collection({ siteId: '64b6afc54c1a9ceff92d8f2b', collectionId: '64b6afc54c1a9ceff92d8f6b' }).then(collections => console.log(collections));
-
-  // Fetches the items in the collection "Öppettidersplashes"
-  api
-    .items({
+  try {
+    // Fetch items in the collection "Öppettidersplashes"
+    const it = await api.items({
       siteId: "64b6afc54c1a9ceff92d8f2a",
       collectionId: "64b6afc54c1a9ceff92d8f6b",
-    })
-    .then((it) => {
-      // console.log(res.items);
-
-      // get hours for a specific date
-      const thisDate = it.items.filter(
-        (item) => !!item.datum && isSameDay(parseISO(item.datum), new Date())
-      );
-
-      // get hours for a generic date
-      const date = new Date().getDate();
-      let dag = "sondag";
-      switch (date) {
-        case 1:
-          dag = "mandag";
-          break;
-        case 2:
-          dag = "tisdag";
-          break;
-        case 3:
-          dag = "onsdag";
-          break;
-        case 4:
-          dag = "torsdag";
-          break;
-        case 5:
-          dag = "fredag";
-          break;
-        case 6:
-          dag = "lordag";
-          break;
-      }
-      const daySlug = `generell-${dag}`;
-      const genericDay = it.items.filter((item) => item.slug === daySlug);
-
-      // return specific if it exists, or generic if not.
-      if (thisDate[0]) {
-        console.log("thisDate", thisDate[0]["text-som-visas"]);
-        res.json({
-          body: thisDate[0]["text-som-visas"],
-          query: req.query,
-          cookies: req.cookies,
-        });
-      } else {
-        console.log("genericDate", genericDay[0]["text-som-visas"]);
-        res.json({
-          body: genericDay[0]["text-som-visas"],
-          query: req.query,
-          cookies: req.cookies,
-        });
-      }
     });
-};
-module.exports = allowCors(handler);
+
+    // Get hours for a specific date
+    const thisDate = it.items.filter(
+      (item) => !!item.datum && isSameDay(parseISO(item.datum), new Date())
+    );
+
+    // Get hours for a generic date
+    const date = new Date().getDate();
+    let dag = "sondag";
+    switch (date) {
+      case 1:
+        dag = "mandag";
+        break;
+      case 2:
+        dag = "tisdag";
+        break;
+      case 3:
+        dag = "onsdag";
+        break;
+      case 4:
+        dag = "torsdag";
+        break;
+      case 5:
+        dag = "fredag";
+        break;
+      case 6:
+        dag = "lordag";
+        break;
+    }
+    const daySlug = `generell-${dag}`;
+    const genericDay = it.items.filter((item) => item.slug === daySlug);
+
+    // Return specific if it exists, or generic if not.
+    if (thisDate[0]) {
+      console.log("thisDate", thisDate[0]["text-som-visas"]);
+      res.json({
+        body: thisDate[0]["text-som-visas"],
+        query: req.query,
+        cookies: req.cookies,
+      });
+    } else {
+      console.log("genericDate", genericDay[0]["text-som-visas"]);
+      res.json({
+        body: genericDay[0]["text-som-visas"],
+        query: req.query,
+        cookies: req.cookies,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+}
